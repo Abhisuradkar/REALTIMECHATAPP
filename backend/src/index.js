d3782cebabcf5ad11da73a6688313776686388a5
Abhishek Suradkar <suradkarabhishek57@gmail.com>
2025-11-15 11:44:55 +0530

    Added login feature

--- backend/src/index.js
+++ backend/src/index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendPath));
 
   // Catch-all route for React router
-  app.get("*", (req, res) => {
+  app.get( (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
